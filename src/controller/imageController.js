1bb237917380f76a313d0c8046ff08dba8d34fd5
Маліцька Наталя <91392018+Natali27M@users.noreply.github.com>
2023-04-12 20:53:51 +0300

    Create image

--- src/controller/imageController.js
+++ src/controller/imageController.js
@@ -2,7 +2,8 @@ const {imageService} = require('../services');
 
 module.exports = {
     getImageUrl: async (req, res, next) => {
-        const imageUrl = await imageService.getImageUrl();
+        const imageUrl = await imageService.getImageUrl(req.body.transcript);
+        console.log(imageUrl,'111111111111111111111111111111111');
         return res.json(imageUrl);
     }
 };
--- src/services/imageService.js
+++ src/services/imageService.js
@@ -1,12 +1,33 @@
 const axios = require('axios');
-// const Replicate = require('replicate');
+// import { parseFile } from 'music-metadata';
+// import { inspect } from 'util';
+
+const { Configuration, OpenAIApi } = require("openai");
 
 module.exports = {
-    getImageUrl: async (res) => {
+    // getText: async () => {
+    //     const configuration = new Configuration({
+    //         apiKey: "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",
+    //         // apiKey: "YOUR_OPENAI_API_KEY",
+    //     });
+    //     const openai = new OpenAIApi(configuration);
+    //
+    //     async function transcribeAudio(filename) {
+    //         const transcript = await openai.createTranscription(
+    //             fs.createReadStream(filename),
+    //             "whisper-1"
+    //         );
+    //         // console.log(transcript.data.text,'-----------------------');
+    //         return transcript.data.text;
+    //     }
+    // },
+
+    getImageUrl: async (text) => {
         try {
             const axiosResponse = await axios.post('https://api.replicate.com/v1/predictions', {
                 "version": "25d2f75ecda0c0bed34c806b7b70319a53a1bccad3ade1a7496524f013f48983",
-                "input": {"prompt": "frog portrait wear white coat and black headphone and 3D-glasses, #pixel"}
+                "input": {"prompt": `${text}`}
+                // "input": {"prompt": "frog portrait wear white coat and black headphone and 3D-glasses, #pixel"}
             }, {
                 headers: {
                     Authorization: `Token e80e4cb2a5e55d7b8b3b776f9010640c9ce28042`,
@@ -22,14 +43,15 @@ module.exports = {
                 });
                 // const myImage = imageUrl.data.output;
                 console.log(imageUrl.data.output, '+++++++++++++++++++++++++++++');
+                // return {image: imageUrl.data.output};
                 // if (myImage) {
-                    return await imageUrl.data.output;
+                //     return res.json(imageUrl.data.output);
                 // }
             }, "5000");
 
-            // console.log(imageUrl, 'OBJECT');
+            console.log(urlImage, 'OBJECT');
             // if (imageUrl.data.output) {
-            return res.json(urlImage);
+            // return res.json(urlImage);
             // }
         } catch (e) {
             console.log(e);
